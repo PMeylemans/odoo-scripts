7393f70ca32050a3dcbc2abadc850a0a90dce402
PMeylemans <52534567+PMeylemans@users.noreply.github.com>
2021-11-05 16:56:06 +0100

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+freeze=3
+
+apt-get install adduser -y
+sleep $freeze
+apt-get install fonts-inconsolata -y
+sleep $freeze
+apt-get install fonts-font-awesome -y
+sleep $freeze
+apt-get install fonts-roboto-unhinted -y
+sleep $freeze
+apt-get install libjs-underscore -y
+sleep $freeze
+apt-get install lsb-base -y
+sleep $freeze
+apt-get install postgresql-client -y
+sleep $freeze
+apt-get install python3-babel -y
+sleep $freeze
+apt-get install python3-chardet -y
+sleep $freeze
+apt-get install python3-dateutil -y
+sleep $freeze
+apt-get install python3-decorator -y
+sleep $freeze
+apt-get install python3-docutils -y
+sleep $freeze
+apt-get install python3-feedparser -y
+sleep $freeze
+apt-get install python3-freezegun -y
+sleep $freeze
+apt-get install python3-html2text -y
+sleep $freeze
+apt-get install python3-pil -y
+sleep $freeze
+apt-get install python3-jinja2 -y
+sleep $freeze
+apt-get install python3-libsass -y
+sleep $freeze
+apt-get install python3-lxml -y
+sleep $freeze
+apt-get install python3-mako -y
+sleep $freeze
+apt-get install python3-mock -y
+sleep $freeze
+apt-get install python3-passlib -y
+sleep $freeze
+apt-get install python3-polib -y
+sleep $freeze
+apt-get install python3-psutil -y
+sleep $freeze
+apt-get install python3-psycopg2 -y
+sleep $freeze
+apt-get install python3-pydot -y
+sleep $freeze
+apt-get install python3-pyparsing -y
+sleep $freeze
+apt-get install python3-pypdf2 -y
+sleep $freeze
+apt-get install python3-qrcode -y
+sleep $freeze
+apt-get install python3-reportlab -y
+sleep $freeze
+apt-get install python3-requests -y
+sleep $freeze
+apt-get install python3-stdnum -y
+sleep $freeze
+apt-get install python3-suds -y
+sleep $freeze
+apt-get install python3-tz -y
+sleep $freeze
+apt-get install python3-vatnumber -y
+sleep $freeze
+apt-get install python3-vobject -y
+sleep $freeze
+apt-get install python3-werkzeug -y
+sleep $freeze
+apt-get install python3-xlsxwriter -y
+sleep $freeze
+apt-get install python3-xlrd -y
+sleep $freeze
+apt-get install python3-ldap -y 
+sleep $freeze
+apt-get install python3-gevent -y             
